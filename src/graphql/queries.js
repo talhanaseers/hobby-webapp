20376d8a9e34d64c22a79b1945791a5385eb989e
Talha Naseer <talha.naseer@ku,edu>
2024-09-29 02:27:03 -0500

    Adding profile

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,35 +1,12 @@
-/* eslint-disable */
-// this is an auto generated file. This will be overwritten
+import { gql } from 'apollo-boost';
 
-export const getTodo = /* GraphQL */ `
-  query GetTodo($id: ID!) {
-    getTodo(id: $id) {
-      id
-      name
-      description
-      createdAt
-      updatedAt
-      __typename
+export const getUser = gql`
+    query GetUser($id: ID!) {
+        getUser(id: $id) {
+            id
+            name
+            email
+            // Add other fields you need
+        }
     }
-  }
-`;
-export const listTodos = /* GraphQL */ `
-  query ListTodos(
-    $filter: ModelTodoFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        name
-        description
-        createdAt
-        updatedAt
-        __typename
-      }
-      nextToken
-      __typename
-    }
-  }
 `;
